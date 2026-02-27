cd9ccacf36e1ee932193fd6323968a7aa4da9a2a
Aidan Crilly <20111148+aidancrilly@users.noreply.github.com>
2026-02-27 22:31:28 +0000

    Lint

--- tests/test_optimise.py
+++ tests/test_optimise.py
@@ -167,8 +167,8 @@ def test_optimise_singlefidelity_RF(singlefidelitysample, batch_size, generate_a
 
     # since RF is non-deterministic and may not find the best point first time
     assert (
-    np.isclose(new_state.best_value, state.best_value, atol=0.1) or 
-    new_state.best_value.item() > state.best_value.item()
+        np.isclose(new_state.best_value, state.best_value, atol=0.1)
+        or new_state.best_value.item() > state.best_value.item()
     )
 
 
