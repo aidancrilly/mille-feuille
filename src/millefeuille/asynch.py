86fdf719350a78b39e25b28d5110046c3a9632f0
Aidan Crilly <20111148+aidancrilly@users.noreply.github.com>
2026-04-11 18:07:42 +0100

    Update asynch.py

--- src/millefeuille/asynch.py
+++ src/millefeuille/asynch.py
@@ -264,7 +264,7 @@ class AsyncScheduler:
 
                 # 2. Notify caller and accept new tasks
                 if num_completed > 0 and on_tasks_complete is not None:
-                    new_tasks = on_tasks_complete(state, num_completed)
+                    new_tasks, state = on_tasks_complete(state, num_completed)
                     if new_tasks:
                         self._pending_tasks.extend(new_tasks)
 
@@ -432,7 +432,7 @@ def run_async_loop(
 
             idx_next[0] = idx_new[-1]
             evaluations_launched[0] += n_new
-            return new_tasks
+            return new_tasks, state
 
         return None
 
